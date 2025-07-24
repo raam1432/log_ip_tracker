bed47303c66b58030ff5ef1523e7edda62049bda
raam1432 <onlinecode1137@gmail.com>
2025-07-24 11:23:34 -0700

    log_server.py

--- log_server.py
+++ log_server.py
@@ -1,11 +1,12 @@
+import requests
 from flask import Flask, request
-from datetime import datetime
-
 app = Flask(__name__)
 
 @app.route("/log", methods=["POST"])
 def log():
-    with open("visitor_log.csv", "a") as f:
-        f.write(request.data.decode("utf-8"))
-    print(f"[{datetime.now()}] Visitor Logged")
-    return "OK", 200
+    ip, timestamp = request.data.decode("utf-8").strip().split(',')
+    response = requests.post("https://api.sheetbest.com/sheets/47c67199-ce3f-4b24-964e-3a126012bc6f", json={
+        "IP Address": ip,
+        "Timestamp": timestamp
+    })
+    return "Logged to Google Sheet", 200
